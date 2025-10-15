9a41bc93b54e2dbf8b6b20ebfdf61846e7760b46
odybgs21 <odybagus25@gmail.com>
2025-10-15 20:13:35 +0700

    Pertemuan 2 menambahkan data keluhan

--- warga/views.py
+++ warga/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Warga
+from .models import Warga, Pengaduan
 from django.views.generic import ListView, DetailView
 
 # Create your views here.
@@ -12,3 +12,8 @@ class WargaDetailView(DetailView):
     model = Warga
     template_name = 'warga/warga_detail.html'
     context_object_name = 'warga'
+
+class PengaduanListView(ListView):
+    model = Pengaduan
+    template_name = 'warga/pengaduan_list.html'
+    context_object_name = 'pengaduan'
