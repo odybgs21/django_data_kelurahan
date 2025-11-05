635897d8ee431b892dd86b88b466fc3493899ce1
odybgs21 <odybagus25@gmail.com>
2025-11-05 19:39:11 +0700

    pertemuan 6

--- warga/views.py
+++ warga/views.py
@@ -3,6 +3,9 @@ from .models import Warga, Pengaduan
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .forms import WargaForm, PengaduanForm
+from rest_framework.generics import ListAPIView
+from .serializers import WargaSerializer
+from rest_framework.generics import RetrieveAPIView
 
 # Create your views here.
 class WargaListView(ListView):
@@ -58,3 +61,13 @@ class PengaduanDeleteView(DeleteView):
     model = Pengaduan
     template_name = 'warga/pengaduan_confirm_delete.html'
     success_url = reverse_lazy('pengaduan-list')   
+
+class WargaListAPIView(ListAPIView):
+    queryset = Warga.objects.all()
+    serializer_class = WargaSerializer
+
+class WargaDetailAPIView(RetrieveAPIView):
+    queryset = Warga.objects.all()
+    serializer_class = WargaSerializer
+    lookup_field = 'pk'
+    lookup_url_kwarg = 'warga_id'
