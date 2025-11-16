665fee5aea0500b1d3edd164d5536e39df70a467
odybgs21 <odybagus25@gmail.com>
2025-11-16 13:55:05 +0700

    last commit pliss

--- data_kelurahan/urls.py
+++ data_kelurahan/urls.py
@@ -17,10 +17,21 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
+    path('', include('warga.api_urls')),
     path('admin/', admin.site.urls),
     path('warga/', include('warga.urls')),
     path('api/', include('warga.api_urls')),
     path('api/auth/token', obtain_auth_token, name='api-auth-token'),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
+    # --- URL DOKUMENTASI API ---
+    # Menghasilkan file schema.yml
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Halaman Swagger UI
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    # Halaman Redoc
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
